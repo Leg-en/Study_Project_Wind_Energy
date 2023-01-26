7056e5e738f781b7cf5be04a7b6cb184a038a0bb
e.sterthaus <e.sterthaus@conterra.de>
2023-01-26 17:17:38 +0100

    median added

--- postprocessing/graph_creater.py
+++ postprocessing/graph_creater.py
@@ -1,13 +1,19 @@
+import os
+
 import dill
 import numpy as np
 import pandas as pd
 import plotly.graph_objs as go
-import os
+
+
 
 rolling_factor = 5
 res_path = r"C:\uni_workspace\Study_Project_Wind_Energy\Project_Arrays\saves_smart_repair"
 with open(os.path.join(res_path, "res.dill"), "rb") as file:
     res = dill.load(file)
+
+
+
 
 fitness_vals = []
 for iteration in res.history:
@@ -30,7 +36,6 @@ df = pd.DataFrame.from_dict(d)
 df["Fitness"] = df["Fitness"] * -1
 df2 = df.copy(deep=True)
 
-
 df["std"] = df["Fitness"].rolling(rolling_factor).std()
 df["avg"] = df["Fitness"].rolling(rolling_factor).mean()
 
@@ -45,12 +50,16 @@ max["std"] = max["Fitness"].rolling(rolling_factor).std()
 max["avg"] = max["Fitness"].rolling(rolling_factor).mean()
 
 mean = df2_.mean()
-#mean2 = mean.copy(deep=True)
-mean = pd.concat([mean, df2_.quantile([0.25,0.75]).unstack()], axis=1)
+# mean2 = mean.copy(deep=True)
+mean = pd.concat([mean, df2_.quantile([0.25, 0.75]).unstack()], axis=1)
 mean["std"] = mean["Fitness"].rolling(rolling_factor).std()
 mean["avg"] = mean["Fitness"].rolling(rolling_factor).mean()
 
-#f = pd.concat([mean2, df2_.quantile([0.25,0.75]).unstack()], axis=1)
+median = df2_.median()
+median["std"] = median["Fitness"].rolling(rolling_factor).std()
+median["avg"] = median["Fitness"].rolling(rolling_factor).mean()
+
+# f = pd.concat([mean2, df2_.quantile([0.25,0.75]).unstack()], axis=1)
 
 fig = go.Figure([
     #    go.Scatter(
@@ -80,6 +89,13 @@ fig = go.Figure([
         y=mean['Fitness'],
         mode='lines',
         line=dict(color='rgb(128, 255, 0)'),
+    ),
+    go.Scatter(
+        name='Gewinn Median',
+        x=median.index,
+        y=median['Fitness'],
+        mode='lines',
+        line=dict(color='rgb(255, 153, 255)'),
     ),
 
     go.Scatter(
